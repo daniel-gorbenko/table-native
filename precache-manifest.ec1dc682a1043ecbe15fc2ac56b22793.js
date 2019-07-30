fda3b8e45302663087eb843911df426411d97449
Daniel Gorbenko <busymangorbenko@gmail.com>
2019-07-30 20:28:39 +0300

    Updates

---
+++ precache-manifest.ec1dc682a1043ecbe15fc2ac56b22793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e477829d5b33227c7cb87574281608a9",
+    "url": "/table-native/index.html"
+  },
+  {
+    "revision": "08df8d47763b61d4f585",
+    "url": "/table-native/static/css/main.c8fc8e2a.chunk.css"
+  },
+  {
+    "revision": "f2164d7ef777aefa90c0",
+    "url": "/table-native/static/js/2.86a4382f.chunk.js"
+  },
+  {
+    "revision": "08df8d47763b61d4f585",
+    "url": "/table-native/static/js/main.ac698e0a.chunk.js"
+  },
+  {
+    "revision": "8bc125bc59a0ccb09ba7",
+    "url": "/table-native/static/js/runtime~main.31a7d9dd.js"
+  }
+]);
